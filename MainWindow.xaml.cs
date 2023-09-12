b8605b865aad362d440e209e480ea6f627997f51
Nedzhuk <jopkakota322@outlook.com>
2023-09-12 16:43:04 +0300

    конец

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,7 +14,12 @@ namespace DateB
             try
             {
                 DateTime date = DP.SelectedDate.Value;
-                if (date > DateTime.Now.Date || date == null)
+                if (CalVest.IsChecked == false && CalSlav.IsChecked == false)
+                {
+                    MessageBox.Show("Выберите тип календаря", "Ошибка");
+                    Result.Visibility = Visibility.Collapsed;
+                }
+                else if (date > DateTime.Now.Date || date == null)
                 {
                     MessageBox.Show("Выбрана неверная дата", "Ошибка");
                     Result.Visibility = Visibility.Collapsed;
@@ -25,6 +30,7 @@ namespace DateB
                     ButtonThrow.Visibility = Visibility.Visible;
                     ButtonSolve.Visibility = Visibility.Collapsed;
                     DateBlock.Visibility = Visibility.Collapsed;
+                    Scroll.Visibility = Visibility.Visible;
 
                     int day = 0;
                     int month = 0, year = 0;
@@ -50,14 +56,25 @@ namespace DateB
                         else
                         {
                             DateTime tmp = new DateTime(DateTime.Now.Year, DateTime.Now.Month, date.Day);
-                            day = (DateTime.Now - tmp).Days;
+                            if (DateTime.Now.Day > date.Day) day = DateTime.Now.Day - tmp.Day;
+                            else day = DateTime.Now.Day + (DateTime.DaysInMonth(DateTime.Now.Year, (DateTime.Now.Month - 1)) - tmp.Day) - 1;
                         }
                     }
                     else
                     {
-                        if (DateTime.Now.Day < date.Day) month = DateTime.Now.Month + (12 - date.Month) - 1;
-                        else month = DateTime.Now.Month + (12 - date.Month);
-                        day = DateTime.Now.Day + (DateTime.DaysInMonth(DateTime.Now.Year, (DateTime.Now.Month - 1)) - date.Day);
+                        if((int)DateTime.Now.Month > (int)date.Month)
+                        {
+                            if (DateTime.Now.Day < date.Day) month = DateTime.Now.Month - date.Month - 1;
+                            else month = DateTime.Now.Month - date.Month;
+                        }
+                        else
+                        {
+                            if (DateTime.Now.Day < date.Day) month = DateTime.Now.Month + (12 - date.Month) - 1;
+                            else month = DateTime.Now.Month + (12 - date.Month);
+                        }
+                        
+                        if (DateTime.Now.Day < date.Day) day = DateTime.Now.Day + (DateTime.DaysInMonth(DateTime.Now.Year, (DateTime.Now.Month - 1)) - date.Day) - 1;
+                        else if (DateTime.Now.Day > date.Day) day = DateTime.Now.Day - date.Day;
                     }
 
                     Life.Text = $"{year} лет, {month} месяцев, {day} дней.";
@@ -89,9 +106,11 @@ namespace DateB
                     CountDayLeapYear.Text = countDayLeapYear.ToString();
                     LeapYear.Text = leapYear.ToString();
 
-                    //Китйский календарь работает по-китайски, тк там Новый Год в разные дни... ужасно
+                    //Китайский календарь 
                     if (CalVest.IsChecked == true)
                     {
+                        SlavicCalendar.Visibility = Visibility.Collapsed;
+                        EasternCalendar.Visibility = Visibility.Visible;
                         switch (date.Year % 12)
                         {
                             case 0:
@@ -133,6 +152,31 @@ namespace DateB
 
                         }
                     }
+                    //Древневосточный календарь
+                    else if (CalSlav.IsChecked == true) 
+                    {
+                        EasternCalendar.Visibility = Visibility.Collapsed;
+                        SlavicCalendar.Visibility = Visibility.Visible;
+
+                        if ((int)date.Month == 12 && (int)date.Day >= 24 || (int)date.Month == 1 && (int)date.Day <= 30) YearGood.Text = " Мороз";
+                        else if ((int)date.Month == 1 && (int)date.Day >= 31 || (int)date.Month == 2 && (int)date.Day <= 28) YearGood.Text = " Велес";
+                        else if ((int)date.Month == 3) YearGood.Text = " Макошь";
+                        else if ((int)date.Month == 4) YearGood.Text = " Жива";
+                        else if ((int)date.Month == 5 && ((int)date.Day >= 1 || (int)date.Day <= 14)) YearGood.Text = " Ярило";
+                        else if ((int)date.Month == 5 && (int)date.Day >= 15 || (int)date.Month == 6 && (int)date.Day <= 2) YearGood.Text = " Леля";
+                        else if ((int)date.Month == 6 && ((int)date.Day >= 3 || (int)date.Day <= 12)) YearGood.Text = " Кострома";
+                        else if ((int)date.Month == 6 && (int)date.Day >= 13 || (int)date.Month == 7 && (int)date.Day <= 6) YearGood.Text = " Додола";
+                        else if ((int)date.Month == 7 && ((int)date.Day >= 7 || (int)date.Day <= 31) && (int)date.Day != 24) YearGood.Text = " Лада";
+                        else if ((int)date.Month == 7 && (int)date.Day == 24) YearGood.Text = " Иван Купала";
+                        else if ((int)date.Month == 8 && ((int)date.Day >= 1 || (int)date.Day <= 28)) YearGood.Text = " Перун";
+                        else if ((int)date.Month == 8 && (int)date.Day >= 29 || (int)date.Month == 9 && (int)date.Day <= 13) YearGood.Text = " Сева";
+                        else if ((int)date.Month == 9 && ((int)date.Day >= 14 || (int)date.Day <= 28)) YearGood.Text = " Рожаница";
+                        else if ((int)date.Month == 9 && (int)date.Day >= 28 || (int)date.Month == 10 && (int)date.Day <= 15) YearGood.Text = " Сварожичи";
+                        else if ((int)date.Month == 10 && (int)date.Day >= 16 || (int)date.Month == 11 && (int)date.Day <= 8) YearGood.Text = " Морена";
+                        else if ((int)date.Month == 11 && ((int)date.Day >= 9 || (int)date.Day <= 28)) YearGood.Text = " Зима";
+                        else if ((int)date.Month == 11 && (int)date.Day >= 29 || (int)date.Month == 12 && (int)date.Day <= 23) YearGood.Text = " Карачун";
+
+                    }
                 }
             }
             catch
@@ -151,7 +195,7 @@ namespace DateB
             ButtonSolve.Visibility = Visibility.Visible;
             ButtonThrow.Visibility = Visibility.Collapsed;
             DateBlock.Visibility = Visibility.Visible;
-
+            Scroll.Visibility = Visibility.Collapsed;
         }
     }
 }
