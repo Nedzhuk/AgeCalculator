5fb54c3c46f3752a83a45c21be0634aa9fe46a85
Nedzhuk <jopkakota322@outlook.com>
2023-09-12 14:56:42 +0300

    исправлен подсчет возраста

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,13 +14,18 @@ namespace DateB
             try
             {
                 DateTime date = DP.SelectedDate.Value;
-                if (date >= DateTime.Now.Date)
+                if (date > DateTime.Now.Date || date == null)
                 {
                     MessageBox.Show("Выбрана неверная дата", "Ошибка");
                     Result.Visibility = Visibility.Collapsed;
                 }
                 else
                 {
+                    Calendar.Visibility = Visibility.Collapsed;
+                    ButtonThrow.Visibility = Visibility.Visible;
+                    ButtonSolve.Visibility = Visibility.Collapsed;
+                    DateBlock.Visibility = Visibility.Collapsed;
+
                     int day = 0;
                     int month = 0, year = 0;
                     for (int i = 0; i <= DateTime.Now.Year - date.Year; i++)
@@ -34,30 +39,39 @@ namespace DateB
                             else if (date.Year + i < DateTime.Now.Year - 1) year++;
                         }
                     }
-                    day = (DateTime.Now - date).Days;
-                    month = 12 * year;
-                    if (month == 0)
-                    {
-                        if (date.Day <= DateTime.Now.Day) month += DateTime.Now.Month + (12 - date.Month);
-                        else if (date.Day > DateTime.Now.Day) month += DateTime.Now.Month + (12 - date.Month) - 1;
-                    }
-                    else if (date.Month < DateTime.Now.Month) month += DateTime.Now.Month - date.Month;
-                    else if (date.Month > DateTime.Now.Month) month += (12 - date.Month) + DateTime.Now.Month;
 
-                    Years.Text = year.ToString();
-                    Month.Text = month.ToString();
-                    Days.Text = day.ToString();
+                    if (year == 0)
+                    {
+                        if (DateTime.Now.Day < date.Day) month = DateTime.Now.Month - date.Month - 1;
+                        else month = DateTime.Now.Month - date.Month;
+
+                        if (month == 0 && date.Day != DateTime.Now.Day) day = (DateTime.Now - date).Days;
+                        else if (date.Day == DateTime.Now.Day) day = 0;
+                        else
+                        {
+                            DateTime tmp = new DateTime(DateTime.Now.Year, DateTime.Now.Month, date.Day);
+                            day = (DateTime.Now - tmp).Days;
+                        }
+                    }
+                    else
+                    {
+                        if (DateTime.Now.Day < date.Day) month = DateTime.Now.Month + (12 - date.Month) - 1;
+                        else month = DateTime.Now.Month + (12 - date.Month);
+                        day = DateTime.Now.Day + (DateTime.DaysInMonth(DateTime.Now.Year, (DateTime.Now.Month - 1)) - date.Day);
+                    }
+
+                    Life.Text = $"{year} лет, {month} месяцев, {day} дней.";
                     Result.Visibility = Visibility.Visible;
 
                     DayWeek.Text = date.DayOfWeek.ToString();
                     int dayOfWeek = 0;
                     int countDayLeapYear = 0;
                     string leapYear = "";
-                    for(int i = date.Year; i <= date.Year + year; i++)
+                    for (int i = date.Year; i <= date.Year + year; i++)
                     {
                         DateTime tmpDate = new DateTime(i, date.Month, date.Day);
                         if (tmpDate.DayOfWeek == date.DayOfWeek) dayOfWeek++;
-                        if(i % 4 == 0)
+                        if (i % 4 == 0)
                         {
                             if (i % 100 != 0)
                             {
@@ -74,6 +88,51 @@ namespace DateB
                     DayWeekAll.Text = dayOfWeek.ToString();
                     CountDayLeapYear.Text = countDayLeapYear.ToString();
                     LeapYear.Text = leapYear.ToString();
+
+                    //Китйский календарь работает по-китайски, тк там Новый Год в разные дни... ужасно
+                    if (CalVest.IsChecked == true)
+                    {
+                        switch (date.Year % 12)
+                        {
+                            case 0:
+                                YearAnimal.Text = " обезьяна";
+                                break;
+                            case 1:
+                                YearAnimal.Text = " петух";
+                                break;
+                            case 2:
+                                YearAnimal.Text = " собака";
+                                break;
+                            case 3:
+                                YearAnimal.Text = " свинья (кабан";
+                                break;
+                            case 4:
+                                YearAnimal.Text = " крыса (мышь)";
+                                break;
+                            case 5:
+                                YearAnimal.Text = " бык";
+                                break;
+                            case 6:
+                                YearAnimal.Text = " тигр";
+                                break;
+                            case 7:
+                                YearAnimal.Text = " кролик (кот)";
+                                break;
+                            case 8:
+                                YearAnimal.Text = " дракон";
+                                break;
+                            case 9:
+                                YearAnimal.Text = " змея";
+                                break;
+                            case 10:
+                                YearAnimal.Text = " лошадь";
+                                break;
+                            case 11:
+                                YearAnimal.Text = " коза (овца)";
+                                break;
+
+                        }
+                    }
                 }
             }
             catch
@@ -81,7 +140,18 @@ namespace DateB
                 Result.Visibility = Visibility.Collapsed;
                 MessageBox.Show("Выбрана неверная дата", "Ошибка");
             }
-            
+
+        }
+
+        private void Throw(object sender, RoutedEventArgs e)
+        {
+            Result.Visibility = Visibility.Collapsed;
+            Calendar.Visibility = Visibility.Visible;
+            DP.SelectedDate = null;
+            ButtonSolve.Visibility = Visibility.Visible;
+            ButtonThrow.Visibility = Visibility.Collapsed;
+            DateBlock.Visibility = Visibility.Visible;
+
         }
     }
 }
